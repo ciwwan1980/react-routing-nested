fb7dd8e1f5fd679574ff81d2124d6e3b2c6ae04a
Ajil Abdalqader <ajilqado@gmail.com>
2020-05-25 00:41:26 +0200

    passing-data-to-ProductDetails

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import {NavLink} from "react-router-dom"
 
 export default class ProductDetails extends Component {
     render() {
+
+        //?????? weher we got the this.props.product
+
+        const {name,slug,price, shortDescription}= this.props.product
+
         return (
-            <div>
-                
-            </div>
+            // ?????? why we put here Slug, whats the different between productDetails and ProductsPage
+            
+            <NavLink to={`/products/${slug}`} >
+         <tr>
+        <td>{name}</td>
+        <td>{shortDescription}</td>
+        <td>{price}</td>
+         </tr>
+         </NavLink>
         )
     }
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react'
+import ProductDetails from "./ProductDetails"; 
+import "./products.css"
+
+
 
 export default class Products extends Component {
 
-    // state={
-    //     products:[...this.props.data]
-    // }
+    state={
+        products:[...this.props.data]
+    }
+
 
     render() {
-   
+        
+        const allProducts= this.props.data&&this.props.data.map((product,i)=>{
+            return(
+                <ProductDetails  key={i} product={product}/>
+            )
+        })
         return (
-            <div>
-                {this.props.data&&this.props.data.map((item)=>{
-                    console.log(item, "here-----item")
-                    return (
-                        <div>
-                        {item.name}
-                        </div>
-                    )
-                })}
+            <div class
+            
+            // here we show only when we prsee on products, we have all information of products
+            
+            
+            Name="products">
+                <main style={{display:"flex", justifyContent:"center", padding:"20px"}}>
+                <div className="btn1">Reset</div>
+                <div className="btn1" onClick={this.asc}>Sort &#8593; </div>
+                <div className="btn1" onClick={this.des}>Sort &#8595;</div>
+                </main>
+
+                <table>
+                <thead>
+                <tr>
+                <th>Name</th>
+                 <th>shortDecription</th>
+                  <th>Price</th>
+                </tr>
+                </thead> 
+                
+                <tbody>
+                {allProducts}
+                </tbody>
+                </table>
+               
+               
+               
             </div>
         )
     }
